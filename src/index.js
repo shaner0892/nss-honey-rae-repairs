bcadcc7c756ecacd7b70b36a9547d72107c51d9c
Hilary Shaner <hilaryshaner@gmail.com>
2022-02-28 09:44:56 -0600

    end of book 7

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Repairs } from './components/Repairs';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Repairs />
-    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
